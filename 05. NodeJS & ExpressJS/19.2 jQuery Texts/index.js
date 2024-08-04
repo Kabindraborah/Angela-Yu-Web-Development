2b8d1045b09309b364d7cbde6e342b3fbb812b3e
Kabindra Borah <kabindraborah68@gmail.com>
2024-08-04 11:31:30 +0530

    Delete 05. NodeJS & ExpressJS directory

--- 05. NodeJS & ExpressJS/19.2 jQuery Texts/index.js
+++
@@ -1,8 +0,0 @@
-// Manipulate text
-$("h1").text("Bye");
-
-// Manipulate all buttons
-$("button").text("Don't click")
-
-// Manipulate innerHTML (excluding tags)
-$("button").html("<em>New click</em>");
--- 05. NodeJS & ExpressJS/19.3 jQuery Attributes/index.js
+++
@@ -1,8 +0,0 @@
-// Get attributes
-console.log($("img").attr("src"));
-
-// Set attributes
-$("a").attr("href", "https://www.bing.com");
-
-// Class is also an attribute
-console.log($("h3").attr("class"));
